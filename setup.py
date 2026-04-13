e327336df5ad127656874113607b83d124bfb63b
Ankita Panda <adarst1808@gmail.com>
2026-04-13 14:57:18 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Ankita Panda",
+    author_email="ankitapanda.algobrainai@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
